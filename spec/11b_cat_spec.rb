0bcf8d9f259b122538c8b0c854f6a61d308632a7
Olly Seber <olly_s@hotmail.co.uk>
2022-10-31 11:42:10 -0700

    Complete 11_shared_example_spec

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,21 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
+  subject(:hiccup) { described_class.new(name: 'hiccup', color: 'white') }
 
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'when Cat is a child class of Pet' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(hiccup).not_to be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(hiccup).to be_hiding
   end
 end
